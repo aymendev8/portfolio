e987a33111e71861c7abbf4b6436f9cf87c82bd9
aymendev8 <aymenkadri5@gmail.com>
2023-02-19 16:44:04 +0100

    ajout du component home

---
+++ src/components/home/social.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const social = () => {
+  return (
+    <div className="home__social">
+        <a href="https://www.linkedin.com/in/aymen-kadri-173083227/" className="home__social-icon" target='_blank'>
+            <i className="uil uil-linkedin-alt"></i>
+        </a>
+        <a href="https://github.com/aymendev8/" className="home__social-icon" target='_blank'>
+            <i className="uil uil-github-alt"></i>
+        </a>
+        <a href="https://twitter.com/AymenKadri_" className="home__social-icon" target='_blank'>
+            <i className="uil uil-twitter-alt"></i>
+        </a>
+    </div>
+    
+  )
+}
+
+export default social
